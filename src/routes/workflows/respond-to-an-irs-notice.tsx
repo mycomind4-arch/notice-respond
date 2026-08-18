e3d1b808fcc70b0af7052f93f816d36f513a5a70
mycomind4-arch <mycomind4@gmail.com>
2026-08-17 22:30:25 -0700

    Add IRS notice search-intent workflow page

---
+++ src/routes/workflows/respond-to-an-irs-notice.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from "@tanstack/react-router";
+import { SiteFooter } from "@/components/site-footer";
+import { SiteHeader } from "@/components/site-header";
+import { NOTICE_WORKFLOWS, WorkflowHead, WorkflowPage, WorkflowStructuredData } from "@/components/notice-workflow-directory";
+
+const workflow = NOTICE_WORKFLOWS.find((item) => item.slug === "irs-notice")!;
+
+export const Route = createFileRoute("/workflows/respond-to-an-irs-notice")({
+  head: () => ({ ...WorkflowHead({ workflow }), links: [{ rel: "canonical", href: "/workflows/respond-to-an-irs-notice" }], scripts: [WorkflowStructuredData({ workflow })] }),
+  component: () => <><SiteHeader /><WorkflowPage workflow={workflow} /><SiteFooter /></>,
+});
