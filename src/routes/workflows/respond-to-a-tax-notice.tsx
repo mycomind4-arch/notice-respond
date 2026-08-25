0841ca0c0e5a835d33361d54ef4cee1dbaf1f5a7
mycomind4-arch <mycomind4@gmail.com>
2026-08-25 11:25:35 -0700

    Use authority page for tax notice SEO route

--- src/routes/workflows/respond-to-a-tax-notice.tsx
+++ src/routes/workflows/respond-to-a-tax-notice.tsx
@@ -1,10 +1,11 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
-import { NOTICE_WORKFLOWS, WorkflowHead, WorkflowPage, WorkflowStructuredData } from "@/components/notice-workflow-directory";
+import { NOTICE_WORKFLOWS } from "@/components/notice-workflow-directory";
+import { AuthorityWorkflowHead, AuthorityWorkflowPage, AuthorityWorkflowStructuredData } from "@/components/notice-authority-workflow-page";
 
 const workflow = NOTICE_WORKFLOWS.find((item) => item.slug === "tax-notice")!;
 export const Route = createFileRoute("/workflows/respond-to-a-tax-notice")({
-  head: () => ({ ...WorkflowHead({ workflow }), links: [{ rel: "canonical", href: "/workflows/respond-to-a-tax-notice" }], scripts: [WorkflowStructuredData({ workflow })] }),
-  component: () => <><SiteHeader /><WorkflowPage workflow={workflow} /><SiteFooter /></>,
+  head: () => ({ ...AuthorityWorkflowHead({ workflow }), links: [{ rel: "canonical", href: "/workflows/respond-to-a-tax-notice" }], scripts: [AuthorityWorkflowStructuredData({ workflow })] }),
+  component: () => <><SiteHeader /><AuthorityWorkflowPage workflow={workflow} /><SiteFooter /></>,
 });
