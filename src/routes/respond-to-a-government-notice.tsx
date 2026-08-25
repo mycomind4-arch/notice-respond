e7818328665dbc8120f17765e5ab58602c984a7e
mycomind4-arch <mycomind4@gmail.com>
2026-08-25 11:25:29 -0700

    Use authority page for government notice SEO route

--- src/routes/respond-to-a-government-notice.tsx
+++ src/routes/respond-to-a-government-notice.tsx
@@ -1,11 +1,12 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
-import { NOTICE_WORKFLOWS, WorkflowHead, WorkflowPage, WorkflowStructuredData } from "@/components/notice-workflow-directory";
+import { NOTICE_WORKFLOWS } from "@/components/notice-workflow-directory";
+import { AuthorityWorkflowHead, AuthorityWorkflowPage, AuthorityWorkflowStructuredData } from "@/components/notice-authority-workflow-page";
 
 const workflow = NOTICE_WORKFLOWS.find((item) => item.slug === "government-notice")!;
 
 export const Route = createFileRoute("/respond-to-a-government-notice")({
-  head: () => ({ ...WorkflowHead({ workflow }), links: [{ rel: "canonical", href: "/respond-to-a-government-notice" }], scripts: [WorkflowStructuredData({ workflow })] }),
-  component: () => <><SiteHeader /><WorkflowPage workflow={workflow} /><SiteFooter /></>,
+  head: () => ({ ...AuthorityWorkflowHead({ workflow }), links: [{ rel: "canonical", href: "/respond-to-a-government-notice" }], scripts: [AuthorityWorkflowStructuredData({ workflow })] }),
+  component: () => <><SiteHeader /><AuthorityWorkflowPage workflow={workflow} /><SiteFooter /></>,
 });
